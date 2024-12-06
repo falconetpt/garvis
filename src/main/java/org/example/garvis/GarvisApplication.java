e59574bf8fcb594f27d73689a2bd59375e33532e
Ricardo Gomes <rgomes@visa.com>
2024-12-06 15:48:35 +0000

    first commit

---
+++ src/main/java/org/example/garvis/GarvisApplication.java
@@ -0,0 +1,13 @@
+package org.example.garvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarvisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GarvisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/garvis/GarvisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.garvis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarvisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
